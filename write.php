37de54a824b9a36e0bc97fab1f24cf65b287550a
Douglas Hensel <dhensel@metrostarsystems.com>
2014-08-26 10:27:09 -0400

    test: removed white space

--- write.php
+++ write.php
@@ -1,11 +1,8 @@
 <?php
-
 	$filename = $_REQUEST['filename'];
 	$contents = $_REQUEST['contents'];
 
 	$myfile = fopen($filename, "w") or die("Unable to open file!");
 	fwrite($myfile, $contents);
 	fclose($myfile);
-
-
 ?>
