2eb80cc197aa973f315239c082da3d78df08c291
Douglas Hensel <dhensel@metrostarsystems.com>
2014-08-26 11:19:17 -0400

    added comments

--- js/read.js
+++ js/read.js
@@ -1,3 +1,8 @@
+// filename = the file to read from
+// cabllbackFn = the function to call when the data is captured
+//		this is done so that the data can pass from the AJAX back to the proper code
+//		workaround for trying to return data in nested functions
+
 function readJSON( filename, callbackFn ){
 
 	$.ajax({
@@ -7,7 +12,11 @@ function readJSON( filename, callbackFn ){
 	})
 	.done(function( data ) {
 		console.log( "SUCCESS: read file -> " + filename );
-		callbackFn( data );
+		// copied from: http://css-tricks.com/snippets/javascript/check-if-function-exists-before-calling/
+		// test if the call back function exists before trying to execute
+		if (typeof callbackFn == 'function') {
+			callbackFn( data );
+		}
 	})
 	.fail(function( jqXHR, textStatus, errorThrown ){
 		console.log( "FAILED: " + textStatus );
